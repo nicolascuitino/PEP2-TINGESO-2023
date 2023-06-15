f0fe55cc15257fb65d40bc4406e4b819c2a1501d
nicolascuitino <nicolas.cuitino@usach.cl>
2023-06-14 21:06:32 -0400

    config,eureka,gateway

---
+++ backend_tingeso/pagos/src/main/java/com/tingeso/pagos/PagosApplication.java
@@ -0,0 +1,15 @@
+package com.tingeso.pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//@EnableEurekaServer
+public class PagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosApplication.class, args);
+	}
+
+}
